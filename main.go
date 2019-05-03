c4bcb0b7d0cbe714e01d5ded8f6c31b33f7631ea
Hassan Sani <saniyhassan@gmail.com>
2019-05-03 01:41:21 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problem.csv",
+		"a csv file in the format of 'question''answers'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFileName))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		exit("Fialed to parse the provided csv file")
+	}
+
+	problems := paresLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You score %d out of %d. \n", correct, len(problems))
+}
+
+func paresLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
